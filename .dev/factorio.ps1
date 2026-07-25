51f3c2630194cd336c6424ff5f41c5232d428946
Em Karisch <emii.khaos@gmail.com>
2026-07-25 12:03:39 +0200

    chore(dev): simplify mods dir

--- .dev/factorio.ps1
+++ .dev/factorio.ps1
@@ -2,9 +2,9 @@ Set-Variable -Name workspace -Value $Env:FACTORIO_WORKSPACE
 Set-Variable -Name mod_base -Value $Env:FACTORIO_STEAM_MODS_BASE
 Set-Variable -Name package -Value "khaosdev"
 
-New-Item -ItemType Directory -Force -Path "$mod_base\$package" | Out-Null
+New-Item -ItemType Directory -Force -Path "$mod_base" | Out-Null
 
 Set-Variable -Name version -Value (Get-Content "$workspace\$package\info.json" | ConvertFrom-Json).version
-Move-Item -Path "$workspace\$package\${package}_${version}.zip" -Destination "${mod_base}\${package}" -Force -ErrorAction SilentlyContinue
+Move-Item -Path "$workspace\$package\${package}_${version}.zip" -Destination "$mod_base" -Force -ErrorAction SilentlyContinue
 
-Start-Process -FilePath "C:\Program Files (x86)\Steam\steam.exe" -ArgumentList "-applaunch 427520 --mod-directory $mod_base\$package"
+Start-Process -FilePath "C:\Program Files (x86)\Steam\steam.exe" -ArgumentList "-applaunch 427520 --mod-directory $mod_base"
--- .dev/factorio_2.0.ps1
+++ .dev/factorio_2.0.ps1
@@ -2,9 +2,9 @@ Set-Variable -Name workspace -Value $Env:FACTORIO_WORKSPACE
 Set-Variable -Name mod_base -Value $Env:FACTORIO_20_MODS_BASE
 Set-Variable -Name package -Value "khaosdev"
 
-New-Item -ItemType Directory -Force -Path "$mod_base\$package" | Out-Null
+New-Item -ItemType Directory -Force -Path "$mod_base" | Out-Null
 
 Set-Variable -Name version -Value (Get-Content "$workspace\$package\info.json" | ConvertFrom-Json).version
-Move-Item -Path "$workspace\$package\${package}_${version}.zip" -Destination "${mod_base}\${package}" -Force -ErrorAction SilentlyContinue
+Move-Item -Path "$workspace\$package\${package}_${version}.zip" -Destination "$mod_base" -Force -ErrorAction SilentlyContinue
 
-Start-Process -FilePath "${Env:FACTORIO_20_PATH}\bin\x64\Factorio.exe" -ArgumentList "--mod-directory ${mod_base}\${package}"
+Start-Process -FilePath "${Env:FACTORIO_20_PATH}\bin\x64\Factorio.exe" -ArgumentList "--mod-directory $mod_base"
